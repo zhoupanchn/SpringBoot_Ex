bcfa7ea80b06a67af7dd5521b577f5834c14013d
ZPCHN <zhoupanchn@163.com>
2020-04-27 15:42:13 +0800

    搭建Springboot与redis项目集成

---
+++ springboot_redis/src/main/java/com/zpchn/springboot/redis/SpringbootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.zpchn.springboot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisApplication.class, args);
+	}
+
+}
